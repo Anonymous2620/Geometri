315ff887f75a1ce7d7af89449ffc77d05b587cfd
Simranjit Singh Gulati <simr0026@zbc.dk>
2021-02-22 12:09:36 +0100

    Add project files.

---
+++ Geometri/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Geometri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square squ = new Square(103);
+
+            Console.WriteLine($"Omk: {squ.CPerimeter()}");
+
+            Console.WriteLine($"Are: {squ.CArea()}");
+        }
+    }
+}
---
+++ Geometri/Square.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometri
+{
+    public class Square
+    {
+        private double Side { get; set; }
+        public Square(double a)
+        {
+            this.Side = a;
+        }
+
+
+        public double CPerimeter()
+        {
+            return 4 * Side;
+        }
+        //Her ganger jeg med 4 fordi når man har en kvadrat. Finder man omkreds ved at gange antallet af sider med et af siderne.
+
+        public double CArea()
+        {
+            return Side * Side;
+        }
+        //Her ganger jeg tallet med sig selv da det er en og alle sider er lige lange.
+    }
+}
